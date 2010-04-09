64721319818920aca807f0035c3cca85a4f06beb
Chris Smith <toonarmy@phpbb.com>
2010-04-09 16:08:11 +0200

    [feature/cache-tests] Whoops wrong method name.

--- tests/cache/cache.php
+++ tests/cache/cache.php
@@ -12,10 +12,10 @@ require_once '../phpBB/includes/functions.php';
 
 class phpbb_cache_test extends phpbb_test_case
 {
-	public function new_cache($type)
+	public function new_acm($type)
 	{
 		$this->init_test_case_helpers();
-		return $this->test_case_helpers->new_cache($type);
+		return $this->test_case_helpers->new_acm($type);
 	}
 
 	public function getDataSet()
@@ -25,7 +25,7 @@ class phpbb_cache_test extends phpbb_test_case
 
 	public function test_get()
 	{
-		$cache = $this->new_cache('null');
+		$cache = $this->new_acm('null');
 	
 		$this->assertFalse($this->get(md5('doNotExist:' . rand(0, PHP_INT_MAX))));
 	}
